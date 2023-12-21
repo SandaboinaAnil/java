f970dc3f0c78d45c317f1179dc35342bcc3b1aad
SandaboinaAnil <anilsandaboina357@gmail.com>
2023-12-21 11:03:08 +0530

    new file

---
+++ Fact.java
@@ -0,0 +1,15 @@
+class Fact
+{
+	public static void main(String args[])
+	{
+		int num=8,num1=1;
+		while(num>=num1)
+		{
+			if(num%num1==0)
+			{
+				System.out.println(num1);
+			}
+			num1++;
+		}
+	}
+}
